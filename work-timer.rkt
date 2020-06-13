dbce8cfe71719c460e6721134ff531847cb4a01c
Gestalte <philipbenade@gmail.com>
2020-06-13 14:36:05 +0200

    Changed the GUI layout so that it no longer requires hard coded spacer strings.

--- work-timer.rkt
+++ work-timer.rkt
@@ -2,12 +2,13 @@
 
 (require racket/gui)
 
-(define gauge-increment (lambda ()
-                 (minutes-to-miliseconds 1)))
+(define gauge-increment
+  (lambda ()
+    (minutes-to-miliseconds 1)))
 
 (define minutes-to-miliseconds
   (lambda (minutes)
-    (* minutes 60000)))
+    (* minutes 6)))
 
 (define (work-time) 25)
 (define (break-time) 5)
@@ -38,28 +39,40 @@
   (make-timer (lambda ()
                 (timer-callback long-break-msg "Not on long break" "30 Minutes have passed, back to work."))))
 
-; Make a frame by instantiating the frame% class
-(define frame (new frame% [label "Work Timer"]))
+(define frame (new frame%
+                   [label "Work Timer"]
+                   [stretchable-width #f]	 
+                   [stretchable-height #f]))
 
-; placeholder string that is used to make sure there is enough space in the UI to display full
-; messages.
-(define spacer-string "000000000000000000000000000")
-
-; Add a horizontal panel to the dialog, with centering for buttons
-(define work-panel (new horizontal-panel% [parent frame]
-                        [alignment '(center center)]))
-
-(define break-panel (new horizontal-panel% [parent frame]
+(define timer-panel (new horizontal-panel%
+                         [parent frame]
                          [alignment '(center center)]))
 
-(define long-break-panel (new horizontal-panel% [parent frame]
-                              [alignment '(center center)]))
+(define timer-button-panel (new horizontal-panel%
+                                [parent timer-panel]
+                                [alignment '(center center)]
+                                [stretchable-width #f]))
 
-(define checkbox-heading-panel (new horizontal-panel% [parent frame]
-                              [alignment '(center center)]))
+(define timer-button-left-panel (new vertical-panel%
+                                     [parent timer-button-panel]
+                                     [alignment '(left center)]))
 
-(define checkbox-panel (new horizontal-panel% [parent frame]
-                              [alignment '(center center)]))
+(define timer-button-right-panel (new vertical-panel%
+                                      [parent timer-button-panel]
+                                      [alignment '(right center)]))
+
+(define timer-message-panel (new vertical-panel%
+                                [parent timer-panel]
+                                [alignment '(left center)]
+                                [min-width 150]))
+
+(define checkbox-heading-panel (new horizontal-panel%
+                                    [parent frame]
+                                    [alignment '(center center)]))
+
+(define checkbox-panel (new horizontal-panel%
+                            [parent frame]
+                            [alignment '(center center)]))
 
 (define start-gauge
   (lambda (range)
@@ -75,104 +88,104 @@
     (send gauge-timer stop)
     (send gauge set-value 0)))
 
-; Make a button in the frame
-(void (new button% [parent work-panel]
-     [label "     Work     "]
-     ; Callback procedure for a button click:
-     [callback (lambda (button event)
-                 (start-gauge (work-time))
-                 (send work-msg set-label "Work timer started")
-                 (send work-timer start (minutes-to-miliseconds (work-time)) #t))]))
+(void (new button%
+           [parent timer-button-left-panel]
+           [label "     Work     "]
+           [callback (lambda (button event)
+                       (start-gauge (work-time))
+                       (send work-msg set-label "Work timer started")
+                       (send work-timer start (minutes-to-miliseconds (work-time)) #t))]))
 
-(void (new button% [parent work-panel]
-     [label "Cancel"]
-     [callback (lambda (button event)
-                 (send work-msg set-label "Not working")
-                 (reset-gauge)
-                 (send work-timer stop))]))
+(void (new button%
+           [parent timer-button-right-panel]
+           [label "Cancel"]
+           [callback (lambda (button event)
+                       (send work-msg set-label "Not working")
+                       (reset-gauge)
+                       (send work-timer stop))]))
 
-; Make a static text message in the frame
-(define work-msg (new message% [parent work-panel]
-                      [label spacer-string]))
+(define work-msg (new message%
+                      [parent timer-message-panel]
+                      [auto-resize #t]
+                      [label"Not working"]))
 
-; Make a button in the frame
-(void (new button% [parent break-panel]
-     [label "     Break     "]
-     ; Callback procedure for a button click:
-     [callback (lambda (button event)
-                 (start-gauge (break-time))
-                 (send break-msg set-label "Break timer started")
-                 (send break-timer start (minutes-to-miliseconds (break-time)) #t))]))
+(void (new button%
+           [parent timer-button-left-panel]
+           [label "     Break     "]
+           [callback (lambda (button event)
+                       (start-gauge (break-time))
+                       (send break-msg set-label "Break timer started")
+                       (send break-timer start (minutes-to-miliseconds (break-time)) #t))]))
 
-(void (new button% [parent break-panel]
-     [label "Cancel"]
-     [callback (lambda (button event)
-                 (send break-msg set-label "Not on break")
-                 (reset-gauge)
-                 (send break-timer stop))]))
+(void (new button%
+           [parent timer-button-right-panel]
+           [label "Cancel"]
+           [callback (lambda (button event)
+                       (send break-msg set-label "Not on break")
+                       (reset-gauge)
+                       (send break-timer stop))]))
 
-; Make a static text message in the frame
-(define break-msg (new message% [parent break-panel]
-                       [label spacer-string]))
+(define break-msg (new message%
+                       [parent timer-message-panel]
+                       [auto-resize #t]
+                       [label "Not on break"]))
 
-; Make a button in the frame
-(void (new button% [parent long-break-panel]
-     [label "Long Break"]
-     ; Callback procedure for a button click:
-     [callback (lambda (button event)
-                 (start-gauge (long-break-time))
-                 (send long-break-msg set-label "Long break timer started")
-                  (send long-break-timer start (minutes-to-miliseconds (long-break-time)) #t))]))
+(void (new button%
+           [parent timer-button-left-panel]
+           [label "Long Break"]
+           [callback (lambda (button event)
+                       (start-gauge (long-break-time))
+                       (send long-break-msg set-label "Long break timer started")
+                       (send long-break-timer start (minutes-to-miliseconds (long-break-time)) #t))]))
 
-(void (new button% [parent long-break-panel]
-     [label "Cancel"]
-     [callback (lambda (button event)
-                 (send long-break-msg set-label "Not on long break")
-                 (reset-gauge)
-                 (send long-break-timer stop))]))
+(void (new button%
+           [parent timer-button-right-panel]
+           [label "Cancel"]
+           [callback (lambda (button event)
+                       (send long-break-msg set-label "Not on long break")
+                       (reset-gauge)
+                       (send long-break-timer stop))]))
 
-; Make a static text message in the frame
-(define long-break-msg (new message% [parent long-break-panel]
-                            [label spacer-string]))
+(define long-break-msg (new message%
+                            [parent timer-message-panel]
+                            [auto-resize #t]
+                            [label "Not on long break"]))
 
-; Make a static text message in the frame
-(define checkbox-heading (new message% [parent checkbox-heading-panel]
-                      [label "Check off successful work sessions:"]))
+(define checkbox-heading (new message%
+                              [parent checkbox-heading-panel]
+                              [label "Check off successful work sessions:"]))
 
-(define cb1 (new check-box% [label ""]
+(define cb1 (new check-box%
+                 [label ""]
                  [parent checkbox-panel]))
 
-(define cb2 (new check-box% [label ""]
+(define cb2 (new check-box%
+                 [label ""]
                  [parent checkbox-panel]))
 
-(define cb3 (new check-box% [label ""]
+(define cb3 (new check-box%
+                 [label ""]
                  [parent checkbox-panel]))
 
-(define cb4 (new check-box% [label ""]
+(define cb4 (new check-box%
+                 [label ""]
                  [parent checkbox-panel]))
 
-; Make a button in the frame
-(void (new button% [parent checkbox-panel]
-     [label "Reset"]
-     ; Callback procedure for a button click:
-     [callback (lambda (button event)
-                 (send cb1 set-value #f)
-                 (send cb2 set-value #f)
-                 (send cb3 set-value #f)
-                 (send cb4 set-value #f))]))
+(void (new button%
+           [parent checkbox-panel]
+           [label "Reset"]
+           [callback (lambda (button event)
+                       (send cb1 set-value #f)
+                       (send cb2 set-value #f)
+                       (send cb3 set-value #f)
+                       (send cb4 set-value #f))]))
 
 (define gauge (new gauge%
-                   (label "")
-                   (parent frame)
-                   (range (minutes-to-miliseconds 1))))
+                   [label ""]
+                   [parent frame]
+                   [range (minutes-to-miliseconds 1)]))
 
-; Show the frame by calling its show method
 (send frame show #t)
-
-; Set the inital display values for the text labels, this replaces the placeholder string.
-(send work-msg set-label "Not working")
-(send break-msg set-label "Not on break")
-(send long-break-msg set-label "Not on long break")
 
 (define timer-callback
   (lambda (msg-name msg-text message-box-text)    
